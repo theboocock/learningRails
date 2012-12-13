ad88686eddad773027bad92d108e7066e9da2dd8
James Boocock <sfk2001@gmail.com>
2012-12-14 00:58:42 +1300

    Deleted random nob script

--- install_nod.sh
+++
@@ -1,9 +0,0 @@
-su - 
-yum install openssl-devel
-cd /usr/local/src
-wget http://nodejs.org/dist/v0.8.15/node-v0.8.15.tar.gz
-tar zxvf node-v0.8.15.tar.gz
-cd node-v0.8.15
-./configure
-make
-make install
