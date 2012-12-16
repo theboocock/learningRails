6f8c64c6942487b015db22882e21c0f4d1674094
James Boocock <sfk2001@gmail.com>
2012-12-16 18:56:02 +1300

    Added new static app to go with demo_app

---
+++ static_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticApp::Application.config.secret_token = '25fe52ea332caa2b0e0e88a04a224c72fdd7ce8876d7723cd1961ef85fea899255dc305aeee286a18324117332e542fc290e4f93f366fe44d995b1e1e3c9ea9f'
